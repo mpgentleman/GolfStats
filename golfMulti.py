fb04e7844080b453fd613718881adae88b1527d5
mpgentleman <80001401+mpgentleman@users.noreply.github.com>
2023-10-08 20:13:41 -0500

    Update golfMulti.py

--- golfMulti.py
+++ golfMulti.py
@@ -514,7 +514,7 @@ class Practice_Session():
         self.calc_df()
         
     def calc_df(self):
-        shots_input = pd.read_csv('/app/folder/Strokes_to_Hole.csv')
+        shots_input = pd.read_csv("Strokes_to_Hole.csv")
         avg_strokes = pd.read_csv('/app/folder/Avg_Strokes.csv')
         model, poly = create_model_poly(shots_input, 'Feet', 'Avg Shots',2)
         model2 = create_model(avg_strokes, 'Yards', 'Strokes_To_Hole')
